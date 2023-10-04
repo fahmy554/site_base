915b39ead62b2c3cb0e1ed040465e80eaf2e0da2
fahmy554 <drfahmy11@gmail.com>
2023-10-04 14:46:07 +0300

    some message

--- store/models.py
+++ store/models.py
@@ -1,8 +1,15 @@
+import io
 import os
 import pathlib
 from datetime import datetime
 from glob import glob
+from io import StringIO
 
+import PIL
+import requests
+import urllib3
+from PIL import Image
+from django.core.files import File
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.utils.text import slugify
@@ -11,7 +18,7 @@ from my_tennis_club import settings
 from django.templatetags.static import static
 
 # Create your models here.
-
+store_img_folder=os.path.join(settings.BASE_DIR, 'store/static/img/stores_images')
 images_path = os.path.join(settings.BASE_DIR, 'media/stores_images')
 images = glob(images_path + '/*.*')
 
@@ -60,6 +67,10 @@ class Store(models.Model):
         # if not self.slug:
         self.slug = slugify(self.store_name, allow_unicode=True)
         if self.html:
+            p = os.path.join(settings.BASE_DIR, 'store/templates/store/single_store/')
+            name = self.html.replace('adv_', '')
+            p = os.path.join(p, f'{name}.html')
+            pathlib.Path(p).touch(exist_ok=True)
             img = [a for a in images if self.html in a]
             if img:
                 self.store_logo.delete(save=False)
@@ -82,12 +93,53 @@ class Store(models.Model):
 
 class Store_Image(models.Model):
     Store = models.ForeignKey(Store, on_delete=models.CASCADE, verbose_name=_('Store'))
-
-    # StoreImage = models.ImageField(upload_to='stores_images/', verbose_name=_('Image'))
+    img_name = models.CharField(max_length=100, null=True,unique=True)
+    img_url = models.CharField(max_length=500, null=True,blank=True,unique=True,default='')
+    image = models.ImageField(upload_to='stores_images/', verbose_name=_('Image'),null=True)
 
     def __str__(self):
         return str(self.Store)
 
+    def valid_extension(self, _img):
+        if '.jpg' in _img:
+            return "JPEG"
+        elif '.jpeg' in _img:
+            return "JPEG"
+        elif '.png' in _img:
+            return "PNG"
+    def compress_images(self, image):
+        im = Image.open(image)
+        width, height = im.size
+        im = im.resize((width - 50, height - 50), PIL.Image.ANTIALIAS)
+        # crear a BytesIO object
+        im_io = io.BytesIO()
+        im.save(im_io, self.valid_extension(image.name), optimize=True,
+                quality=70)
+        new_image = File(im_io, name=image.name)
+        return new_image
+
+    def download_img(self,url):
+        print(f'downloading img {url}')
+        with open(f'{images_path}/{self.img_name}.jpg', 'wb') as handle:
+            response = requests.get(url, stream=True)
+
+            if not response.ok:
+                print(response)
+
+            for block in response.iter_content(1024):
+                if not block:
+                    break
+
+                handle.write(block)
+        return f'{images_path}/{self.img_name}.jpg'
+    def save(self, *args, **kwargs):
+        if self.img_url:
+            img_=self.download_img(self.img_url)
+            self.image=img_
+        img = Image.open(self.image)
+        img = img.convert('RGB')
+        img.save(fp=os.path.join(store_img_folder,f'{self.img_name}.jpg'),format='JPEG')
+        super().save(*args,**kwargs)
     class Meta:
         verbose_name = _("Store_Image")
         verbose_name_plural = _("Store_Images")
@@ -126,6 +178,8 @@ class Post(models.Model):
         print(f'updating {store.slug} {store.last_modified_date}')
         store.last_modified_date = self.last_modified_date
         print(f'updating {store.slug} {store.last_modified_date}')
+
+
         store.save()
         super().save(*args, **kwargs)
 
